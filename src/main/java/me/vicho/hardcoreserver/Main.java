7d7456e71475f673a791f7ff2f51e3640922a661
xvicho <vicente.veloso@outlook.com>
2025-01-18 12:50:53 -0300

    Cleanup code

--- src/main/java/me/vicho/hardcoreserver/Main.java
+++ src/main/java/me/vicho/hardcoreserver/Main.java
@@ -1,32 +1,16 @@
 package me.vicho.hardcoreserver;
 
-//import org.bukkit.Bukkit;
-//import org.bukkit.World;
-//import org.bukkit.WorldCreator;
-import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
 
-
-//import java.io.File;
-//import java.util.List;
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main extends JavaPlugin {
-//    FileConfiguration config = getConfig();
     private static Main instance;
     @Override
     public void onEnable() {
         instance = this;
-//        limboCheck();
         getServer().getPluginManager().registerEvents(new DeathListener(), this);
         getServer().getPluginManager().registerEvents(new JoinListener(), this);
-//        getServer().getPluginManager().registerEvents(this,this);
         createConfig();
         getLogger().info("HardcoreServer is enabled");
     }
@@ -56,41 +40,4 @@ public class Main extends JavaPlugin {
             e.printStackTrace();
         }
     }
-//    @EventHandler
-//    public void onPlayerJoin(PlayerJoinEvent event) {
-//        if (config.getBoolean("hardcore-finished")) {
-//            event.getPlayer().kickPlayer("Hardcorde has finished");
-//        }
-//    }
-
-
-//    public void limboCheck() {
-//        // test for world limbo present
-//        List<World> Mundos = Bukkit.getWorlds();
-//        boolean limbo_present = false;
-//        for (World var : Mundos) {
-//            if (var.getName() == "limbo") {
-//                limbo_present = true;
-//            }
-//        }
-//        if (limbo_present == false) {
-//            Bukkit.createWorld(new WorldCreator("limbo").generator(new EmptyChunkGenerator()));
-//        }
-//    }
-
-//    public void defaultWorldCreation() {
-//        List<World> Mundos = Bukkit.getWorlds();
-//        boolean default_present = false;
-//        for (World var : Mundos) {
-//            if (var.getName() == "defaultworld") {
-//                default_present = true;
-//            }
-//        }
-//        if (default_present == false) {
-//            Bukkit.createWorld(new WorldCreator("defaultworld").environment(World.Environment.NORMAL));
-//            Bukkit.createWorld(new WorldCreator("defaultworld_nether").environment(World.Environment.NETHER));
-//            Bukkit.createWorld(new WorldCreator("defaultworld_the_end").environment(World.Environment.THE_END));
-//        }
-//
-//    }
 }
